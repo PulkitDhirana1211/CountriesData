9a0cf8babcc14cbfb5546ad045b7417075558214
PulkitDhirana1211 <pulkitdhirana9@gmail.com>
2023-11-26 17:04:13 +0530

    Initial Commit

---
+++ Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Pulkit Dhirana on 26/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
